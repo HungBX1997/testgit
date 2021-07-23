6b05d5758b6fa9a94eae82ef695a9077c0f69eba
hung <hungxuanbui97@gmail.com>
2021-07-23 12:38:11 +0700

    [RM-01] Fixing UpperCase

--- Device/screen.h
+++ Device/screen.h
@@ -3,25 +3,25 @@
 
 typedef struct
 {
-    uint32_t second;
-    uint32_t minute;
+    uint32_t Second;
+    uint32_t Minute;
 }Time_data_t;
 
 typedef struct
 {
-    float dataSpeed;
-    Time_data_t dataTime;
-    float dataDistance;
-    float dataCalo;
-    uint8_t incline;
+    float DataSpeed;
+    Time_data_t DataTime;
+    float DataDistance;
+    float DataCalo;
+    uint8_t Incline;
 }dataUpdate;
 
-void updateData(dataUpdate data);
-void updateTime(uint32_t time);
-void updateDistance(float dis);
-void updateCalo(float cal);
-void updateIncline(uint8_t incl);
-void updateSpeed(float speed);
+void updateData(dataUpdate Data);
+void updateTime(uint32_t Time);
+void updateDistance(float Dis);
+void updateCalo(float Cal);
+void updateIncline(uint8_t Incl);
+void updateSpeed(float Speed);
 
 void mainScreen();
 
